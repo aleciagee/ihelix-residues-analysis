0eb780816e6303e3f2fcf2b432650d2546a8941a
aleciagee <aleciagee>
2025-02-17 14:04:37 +1030

    uniprot active site search

--- src/hmm_search.sh
+++ src/hmm_search.sh
@@ -4,19 +4,19 @@
 # assignments
 DB_NAME="uniprot"
 TEST_NAME="p450_activesite" # only needed for varying uniprot searches
-HMM_NO=1
+TEST_NO=2
 
 
 # parameters
-E_VALUE=10
-# Z_SCORE=497798 # ensure add Z score to options as needed
+E_VALUE=1
+Z_SCORE=700000 # ensure add Z score to options as needed
 # T_SCORE=10 # bit score
 
 
 # locations
 DB_INFILE=../input/databases/uniprot/p450_activesite.fasta # database to query
 HMM_INFILE=../output/hmm_round_1/ihelix_seed_hmm.hmm
-OUTPUT_DIR=../output/hmm_search_${HMM_NO}/${DB_NAME}/${TEST_NAME}/E_VALUE_${E_VALUE}/
+OUTPUT_DIR=../output/hmm_search_1/${DB_NAME}/${TEST_NAME}/${TEST_NO}/
 SCORE_OUTFILE=${OUTPUT_DIR}/ihelixhits_e_${E_VALUE}_z_${Z_SCORE}_T_${T_SCORE}.out
 ALN_OUTFILE=${OUTPUT_DIR}/ihelixhits_e_${E_VALUE}_z_${Z_SCORE}_T_${T_SCORE}.aln
 
@@ -26,6 +26,6 @@ mkdir -p "$OUTPUT_DIR"
 
 ## Querying the HMM profile
 # varying e values
-hmmsearch -A ${ALN_OUTFILE} -E ${E_VALUE} ${HMM_INFILE} ${DB_INFILE} > ${SCORE_OUTFILE}
+hmmsearch -A ${ALN_OUTFILE} -E ${E_VALUE} -Z ${Z_SCORE} ${HMM_INFILE} ${DB_INFILE} > ${SCORE_OUTFILE}
 # varying t score
-# hmmsearch -A -A ${ALN_OUTFILE} -T ${T_SCORE} ${HMM_INFILE} ${DB_INFILE} > ${SCORE_OUTFILE}
+# hmmsearch -A -A ${ALN_OUTFILE} -Z ${Z_SCORE} ${HMM_INFILE} ${DB_INFILE} > ${SCORE_OUTFILE}
