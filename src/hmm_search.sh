35f0732e912a12f5305928e1064f283ccfe3c04d
aleciagee <aleciagee>
2025-02-17 14:34:08 +1030

    updated git ignore for large pfam output

--- src/hmm_search.sh
+++ src/hmm_search.sh
@@ -2,20 +2,20 @@
 
 
 # assignments
-DB_NAME="biocatnet"
+DB_NAME="pfam"
 # TEST_NAME="p450_reviewed_5star" # only needed for varying uniprot searches
-TEST_NO=5
+TEST_NO=1
 
 
 # parameters
-E_VALUE=0.00001
-E_VALUE_NAME="0p00001"
-Z_SCORE=52674 # ensure add Z score to options as needed
+E_VALUE=10
+E_VALUE_NAME="10"
+# Z_SCORE=52674 # ensure add Z score to options as needed
 # T_SCORE=10 # bit score
 
 
 # locations
-DB_INFILE=../input/databases/biocatnet_p450.fasta # database to query
+DB_INFILE=../input/databases/pfam_cytochrome_p450_sequences.fasta # database to query
 HMM_INFILE=../output/hmm_round_1/ihelix_seed_hmm.hmm
 # OUTPUT_DIR=../output/hmm_search_1/${DB_NAME}/${TEST_NAME}/${TEST_NO}/ # for uniprot searches with test_name
 OUTPUT_DIR=../output/hmm_search_1/${DB_NAME}/${TEST_NO}/
@@ -28,6 +28,6 @@ mkdir -p "$OUTPUT_DIR"
 
 ## Querying the HMM profile
 # varying e values
-# hmmsearch -A ${ALN_OUTFILE} -E ${E_VALUE} ${HMM_INFILE} ${DB_INFILE} > ${SCORE_OUTFILE}
+hmmsearch -A ${ALN_OUTFILE} -E ${E_VALUE} ${HMM_INFILE} ${DB_INFILE} > ${SCORE_OUTFILE}
 # varying z value
-hmmsearch -A ${ALN_OUTFILE} -E ${E_VALUE} -Z ${Z_SCORE} ${HMM_INFILE} ${DB_INFILE} > ${SCORE_OUTFILE}
+# hmmsearch -A ${ALN_OUTFILE} -E ${E_VALUE} -Z ${Z_SCORE} ${HMM_INFILE} ${DB_INFILE} > ${SCORE_OUTFILE}
